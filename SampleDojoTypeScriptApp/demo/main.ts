0e73bc3073e8ee1c0131bd376526b4b1e5430600
David Wilton <dwilton@DWILTONW10.ECLLDN.LOCAL>
2015-11-25 08:43:04 +0000

    Removed unused variables

--- SampleDojoTypeScriptApp/demo/main.ts
+++ SampleDojoTypeScriptApp/demo/main.ts
@@ -1,8 +1,5 @@
 import DemoWidget = require("./demoWidget/widget");
 import domConstruct = require("dojo/dom-construct");
-import win = require("dojo/_base/window");
-import DojoTextBox = require("dijit/form/TextBox");
-
 
 export = AppController;
 
